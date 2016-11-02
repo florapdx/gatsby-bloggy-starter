d43e590f21469e3cad72a3587bfce32c9424c2a2
florapdx <floraworleypdx@gmail.com>
2016-11-02 14:30:57 -0700

    try with more than one gist

--- pages/posts/my-third-post.jsx
+++ pages/posts/my-third-post.jsx
@@ -9,6 +9,7 @@ const myThirdPost = props => {
         <p>Fashion axe semiotics marfa 8-bit master cleanse freegan. Vinyl umami before they sold out, polaroid ennui marfa `helvetica` keytar tote bag asymmetrical banh mi glossier. Ugh irony pour-over `XOXO put a bird` on it lumbersexual, tote bag pitchfork sriracha copper mug la croix godard roof party whatever blue bottle.</p>
         <EmbeddedGist gist={'florapdx/f33807aa0cd6ad20fc26d40d63be9e3d'} />
         <p>Fashion axe semiotics marfa 8-bit master cleanse freegan. Vinyl umami before they sold out, polaroid ennui marfa `helvetica` keytar tote bag asymmetrical banh mi glossier. Ugh irony pour-over `XOXO put a bird` on it lumbersexual, tote bag pitchfork sriracha copper mug la croix godard roof party whatever blue bottle.</p>
+        <EmbeddedGist gist={'florapdx/d9b996b63f69818b469d'} />
       </div>
     </JSXPostWrapper>
   );
